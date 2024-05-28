3bbdc0a218ffbc29e31f02d937ad11119e0ca76e
conecttheo <theodamata@gmail.com>
2024-05-28 16:03:39 -0300

    Desafio Concluido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -23,7 +23,7 @@ public class ContaTerminal {
         nomeCliente = scanner.nextLine(); //Obter pela scanner os valores digitados no terminal
 
         System.out.println("Por favor, digite o saldo da Conta!");
-        saldo = scanner.nextDouble(); //Obter pela scanner os valores digitados no terminal.
+        saldo = scanner.nextDouble(); //Obter pela scanner os valores digitados no terminal
 
         // Exibindo a mensagem final
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.%n", 
